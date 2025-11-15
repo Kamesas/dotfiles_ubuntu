d13d157481e2751ef454a3195781f24649910eb6
AlexSemak <semakaleksandr2014@gmail.com>
2025-11-15 15:32:41 +0200

    git confing

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,16 +1,16 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  opts = {
-    current_line_blame = true,
-    current_line_blame_opts = {
-      virt_text = true,
-      virt_text_pos = "eol", -- Show at end of line
-      delay = 100, -- Show blame after 100ms when cursor stops
-      ignore_whitespace = false,
-    },
-    current_line_blame_formatter = "<author>, <author_time:%R> • <summary>",
-    current_line_blame_formatter_opts = {
-      relative_time = true,
-    },
-  },
+	"lewis6991/gitsigns.nvim",
+	opts = {
+		current_line_blame = true,
+		current_line_blame_opts = {
+			virt_text = true,
+			virt_text_pos = "eol", -- Show at end of line
+			delay = 100, -- Show blame after 100ms when cursor stops
+			ignore_whitespace = false,
+		},
+		current_line_blame_formatter = "<author>, <author_time:%R> • <summary>",
+		current_line_blame_formatter_opts = {
+			relative_time = true,
+		},
+	},
 }
