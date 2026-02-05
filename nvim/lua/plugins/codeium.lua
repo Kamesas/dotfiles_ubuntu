545b0c40b709d7b520c77c97a8a5ef71e2df0a18
AlexSemak <semakaleksandr2014@gmail.com>
2026-02-05 19:13:31 +0200

    nvim: codeium fix memory leak

--- nvim/lua/plugins/codeium.lua
+++ nvim/lua/plugins/codeium.lua
@@ -5,20 +5,27 @@ return {
   },
   config = function()
     require("codeium").setup({
-      enable_cmp_source = false, -- Disable cmp source to avoid popup suggestions
+      enable_cmp_source = false,
+      enable_chat = false, -- Disable chat to reduce API calls
+      enable_local_search = false, -- Disable to reduce background activity
+      enable_index_service = false, -- Disable indexing to reduce memory/API usage
       virtual_text = {
-        enabled = true, -- Enable ghost/shadow text
-        manual = false, -- Auto-trigger suggestions
-        idle_delay = 75, -- Delay before showing suggestions
+        enabled = true,
+        manual = false,
+        idle_delay = 150, -- Increased delay to reduce request frequency
         key_bindings = {
-          accept = "<Tab>", -- Accept suggestion with Tab
+          accept = "<Tab>",
           accept_word = false,
           accept_line = false,
-          clear = "<C-]>", -- Clear suggestion with Ctrl-]
-          next = "<M-]>", -- Next suggestion with Alt-]
-          prev = "<M-[>", -- Previous suggestion with Alt-[
+          clear = "<C-]>",
+          next = "<M-]>",
+          prev = "<M-[>",
         },
       },
     })
+
+    vim.keymap.set("n", "<leader>ai", function()
+      vim.cmd("Codeium Toggle")
+    end, { desc = "Toggle Codeium" })
   end,
 }
